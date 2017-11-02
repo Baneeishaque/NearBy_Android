0b5f71145df5703fddce7e0f1cabd17fff489173
Banee Ishaque K <baneeishaque@gmail.com>
2017-11-02 11:19:56 +0530

    Gridlayout

--- app/src/main/java/nears/ashik/ndk/Home.java
+++ app/src/main/java/nears/ashik/ndk/Home.java
@@ -6,6 +6,8 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +39,56 @@ public class Home extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(
+                R.id.recycler_view_grids);
+        recyclerView.addItemDecoration(new MarginDecoration(this));
+        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        GridLayoutManager manager = new GridLayoutManager(this, 3);
+//        manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+//            @Override
+//            public int getSpanSize(int position) {
+////                return (3 - position % 3);
+//                switch (position) {
+//                    case 0:
+//                        return 1;
+//                    case 1:
+//                        return 1;
+//                    case 2:
+//                        return 1;
+//                    case 3:
+//                        return 1;
+//                    case 4:
+//                        return 1;
+//                    case 5:
+//                        return 1;
+//
+//                    case 6:
+//                        return 2;
+//                    case 7:
+//                        return 2;
+//                    case 8:
+//                        return 2;
+//                    case 9:
+//                        return 2;
+//
+//                    default:
+//                        return 1;
+//
+//                }
+//            }
+//        });
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(new NumberedAdapter(6));
+
+        recyclerView = (RecyclerView) findViewById(
+                R.id.recycler_view_grids_3);
+        recyclerView.addItemDecoration(new MarginDecoration(this));
+        recyclerView.setHasFixedSize(true);
+        manager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(new NumberedAdapter(4));
     }
 
     @Override
